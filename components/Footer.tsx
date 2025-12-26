c71ae7b848844da2c28be6da1a87328210921410
unknown <abdurrahmanfaiz187@gmail.com>
2025-12-26 09:21:52 +0700

    build

---
+++ components/Footer.tsx
@@ -0,0 +1,111 @@
+import { Github, Instagram, Linkedin, Twitter } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const socialLinks = [
+  {
+    icon: Twitter,
+    href: "#",
+    label: "Twitter",
+  },
+  {
+    icon: Github,
+    href: "https://github.com/AbdFaiz/",
+    label: "Github",
+  },
+  {
+    icon: Linkedin,
+    href: "https://www.linkedin.com/in/abdurrahman-faiz-dev/",
+    label: "Linkedin",
+  },
+  {
+    icon: Instagram,
+    href: "https://www.instagram.com/4o4nf_",
+    label: "Instagram",
+  },
+];
+
+const quickLinks = [
+  { name: "Home", href: "/" },
+  { name: "Categories", href: "/categories" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
+
+const Footer = () => {
+  return (
+    <footer className="bg-background border-t border-border/50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {/* Brand */}
+          <div className="">
+            <Link
+              href="/"
+              className="text-xl font-serif font-bold text-foreground mb-4 block"
+            >
+              Nx Blog
+            </Link>
+            <p className="text-muted-foreground mb-4">
+              Writing that resonates with the curious minds of the digital age.
+              Exploring the intersectoin of technology, design, and creativity.
+            </p>
+
+            <div className="flex gap-x-4">
+              {socialLinks.map((link) => {
+                const Icon = link.icon;
+                return (
+                  <Link
+                    key={link.label}
+                    href={link.href}
+                    target="_blank"
+                    className="text-muted-foreground hover:text-foreground transition-all duration-150"
+                  >
+                    <Icon className="h-5 w-5" />
+                    <span className="sr-only">{link.label}</span>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+
+              {/* Quick links */}
+          <div className="">
+            <h4 className="font-semibold text-foreground mb-4">Quick Links</h4>
+            <ul className="space-y-2">
+              {quickLinks.map((link) => (
+                <li key={link.name}>
+                  <Link
+                    href={link.href}
+                    className="text-muted-foreground hover:text-foreground transition-all duration-150"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+              {/* Newsletter */}
+          <div className="">
+            <h4 className="font-semibold text-foreground mb-4">Stay Updated</h4>
+            <p className="text-muted-foreground mb-4">
+                Subscribe to get the latest posts delivered to your inbox.
+            </p>
+            <div className="flex flex-col sm:flex-row gap-2">
+                <Input type="email" placeholder="youremail@mail.com" />
+                <Button>Subscribe</Button>
+            </div>
+          </div>
+        </div>
+
+        {/* Copy right */}
+        <div className="mt-12 pt-8 border-t border-border/50 text-center text-sm">
+            <p className="text-muted-foreground">&copy; Nx Blog. All Right reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
